330ace84b5bdde4c60c355b83f3324bb09d0c6c1
Irfan Ansari <irfanansari2k20@gmail.com>
2020-10-10 21:28:34 +0530

    12.Make Header and Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'learnangular';
 
-  getUserValue(values)
-  {
-   console.log(values)
- }
+ 
 }
   
  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {FormsModule} from '@angular/forms'
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+   
     
   ],
   providers: [],
