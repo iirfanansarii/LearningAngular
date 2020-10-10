c884f72a0c9547c2d72b01dab269fee98b858adb
Irfan Ansari <irfanansari2k20@gmail.com>
2020-10-11 00:47:57 +0530

    13.Style binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'learnangular';
 
- 
+  //below defining color property for dynamic style
+  color="orange"
+
+//  below dynminc style function to change color
+changeColor(){
+  this.color="red"
+}
+// below condtional style 
+  err=false;
 }
   
  
