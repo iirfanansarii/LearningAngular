83c64ab927d5ce14308e1320f3f897d7f71994c6
Irfan Ansari <irfanansari2k20@gmail.com>
2020-10-13 01:34:04 +0530

    23. Custom Directives in Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
-import { AdminComponent } from './admin/admin.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CustomStyleDirective } from './custom-style.directive';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
-    AdminComponent,
-    PageNotFoundComponent
+    CustomStyleDirective
+    
   ],
   imports: [
     BrowserModule,
