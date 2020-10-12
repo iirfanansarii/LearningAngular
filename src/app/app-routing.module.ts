5d658978a771ca4c20f8aacedc43df68644502f3
Irfan Ansari <irfanansari2k20@gmail.com>
2020-10-13 00:46:48 +0530

    22. Page not found | angular routing | 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import{ UserComponent } from './user/user.component'
 import{ AdminComponent } from './admin/admin.component'
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component'
 
 const routes: Routes = [
   {
@@ -11,7 +12,15 @@ const routes: Routes = [
   {
     path:'admin',  // this is path for admin componet 
     component: AdminComponent 
+  },
+  // below is third route or path
+  // If you are making a route for that page which  is universally open i.e when a page didn't find (404)
+  // then we use double star and that is also known as  wild card
+  {
+   path:'**' ,
+   component:PageNotFoundComponent
   }
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { AdminComponent } from './admin/admin.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    AdminComponent
+    AdminComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
